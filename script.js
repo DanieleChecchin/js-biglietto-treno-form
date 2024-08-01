e74d92e5da0e3458d0041669d31015ad00505fda
DanieleChecchin <danielechecchin26@gmail.com>
2024-08-01 16:08:25 +0200

    milestone 1

--- script.js
+++ script.js
@@ -2,14 +2,31 @@
 /* SCALETTA
 
 1- Recupero gli elementi dal DOM
+2- Gestisco gli eventi
+3- Stampo in console
 
 */
 
 // ! Fase preparatoria
 
-const kmField = document.querySelector('.age')
-const ageField = document.querySelector('.km')
+// Di base il prezzp non avrà sconto
+let result = null
+
+// Preparo il suffisso dei km
+const kmSuffix = 'km'
+// Preparo il suffisso dell'età
+const ageSuffix = 'anni'
+//Preparo il suffiso 'euro'
+const priceSuffix = '€'
+
+
+const kmField = document.getElementById('kmresult')
+const ageField = document.getElementById('ageresult')
 const button = document.getElementById('generate')
+const ticketElement = document.getElementById('ticket');
+
+
+
 
 // ! Fase gestione eventi
 
@@ -19,8 +36,54 @@ button.addEventListener('click', function (e) {
     const kmValue = kmField.value
     const ageValue = ageField.value
 
+// ! Fase lavorazione dati
+
+// Calcolo il prezzo del biglietto in base ai km
+const ticketPrice = kmValue*0.21;
+console.log(ticketPrice + ' ' + priceSuffix)
+
+ticketPriceHuman = ticketPrice.toFixed(2)
+
+// Calcolo lo sconto in base all'età
+result = ticketPrice
+
+if(ageValue < 18){
+    result = ticketPrice - (ticketPrice / 100*20) //moltiplico per 0.8
+    console.log(result)
+}   else if(ageValue > 65){
+    result = ticketPrice - (ticketPrice / 100*40) //moltiplico per 0.6
+    console.log(result)
+}   
+
+// Imposto 2 cifre decimali per il risultato finale
+resultHuman = result.toFixed(2);
+console.log(resultHuman)    
+
+
+// ! Fase produzione output
+
+// Stabilisco il messaggio relativo ai chilometri da mostrare nel DOM
+const kmresult = `${kmValue} ${kmSuffix}`
+kmresult.innerText = kmresult;
+
+// Stabilisco il messaggio relativo all'età da mostrare nel DOM
+const ageresult = `${ageValue} ${ageSuffix}`
+ageresult.innerText = ageresult;
+
+// Stabilisco il messaggio del prezzo finale da mostrare nel DOM
+const message = `Il costo finale del tuo biglietto è di ${resultHuman} ${priceSuffix}`
+ticketElement.innerText = message;
+
     // Stampo in pagina
     console.log(kmValue)
     console.log(ageValue)
+    console.log(ticketElement)
 
 })
+
+
+
+
+
+
+
